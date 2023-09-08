a6c49584ce4c0f7a0739c4c2bdea7073b58ccc6d
Yuriy Bykov <bykov.yurik.2010@yandex.ru>
2023-09-08 16:00:04 +0300

    01

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 import turtle as t
+import random as r
 
+weight = 700 // 2
+height = 800 // 2
+t.colormode(255)
+t.Screen().bgcolor(0,0,0)
+t.pencolor(100, 100, 100)
+t.tracer(2, 10)
+t.hideturtle()
+def draw_grid():
+    step_grid = 30
+    t.penup()
+    t.setposition(-weight, -height)
+    t.pendown()
+    t.setposition(weight, -height)
+    t.setposition(weight, height)
+    t.setposition(-weight, height)
+    t.setposition(-weight, -height)
+    for y in range(-height + step_grid, height, step_grid):
+        t.setposition(x=-weight, y=y)
+        t.setposition(x=weight, y=y)
+draw_grid()
 t.done()
