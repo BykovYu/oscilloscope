9cef073775257e1f7b5ac2eda36d99ed57ce5a0f
BykovYu <bykov.yurik.2010@yandex.ru>
2023-09-09 12:17:08 +0300

    added transform x and transform y (normal coordinats 0 to 2 * weight)

--- main.py
+++ main.py
@@ -5,20 +5,24 @@ weight = 550 // 2
 height = 550 // 2
 t.colormode(255)
 t.Screen().bgcolor(0,0,0)
-t.pencolor(100, 100, 100)
+t.pencolor(255, 255, 255)
 t.tracer(2, 10)
 t.hideturtle()
+def t_c_x(x):#transform coordinats
+    if x<0: x = -weight
+    elif x>weight * 2 - 1: x = weight
+    else: x = -weight + x
+    return x
+def t_c_y(y):#transform coordinats
+    if y<0: y = -height
+    elif y>weight * 2 - 1: y = weight
+    else: y = -weight + y
+    return y
 def draw_grid(delta):
-    step_grid = height * 2 // 3 - 10
-    delta = delta * (height * 2 // 3 + 3)
     t.penup()
-    t.setposition(x=-weight, y=-height + delta)
+    t.setposition(t_c_x(0), t_c_y(0))
     t.pendown()
-    t.setposition(x=weight, y=-height + delta)
-    t.setposition(x=weight, y=-height + height * 2 // 3 + delta)
-    t.setposition(x=-weight, y=-height + height * 2 // 3 + delta)
-    t.setposition(x=-weight, y=-height + delta)
+    t.setposition(t_c_x(2*weight), t_c_y(2*height))
+    t.setposition(t_c_x(2 * weight), t_c_y(2 * height))
 draw_grid(0)
-draw_grid(1)
-draw_grid(2)
 t.done()
