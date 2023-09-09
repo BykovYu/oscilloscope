1548168b45df2e002e2f97e62854af3c9476684a
BykovYu <bykov.yurik.2010@yandex.ru>
2023-09-09 15:11:26 +0300

    end draw grid

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ weight = 550 // 2
 height = 550 // 2
 t.colormode(255)
 t.Screen().bgcolor(0,0,0)
-t.pencolor(255, 255, 255)
-t.tracer(2, 10)
+t.pencolor(100, 100, 100)
+t.tracer(2, 1)
 t.hideturtle()
 def t_c_x(x):#transform coordinats
     if x<0: x = -weight
@@ -20,9 +20,28 @@ def t_c_y(y):#transform coordinats
     return y
 def draw_grid(delta):
     t.penup()
-    t.setposition(t_c_x(0), t_c_y(0))
+    x0, y0 = t_c_x(0), 0 + t_c_y(delta * (2 * height // 3)) + 3
+    t.setposition(x0, y0)
     t.pendown()
-    t.setposition(t_c_x(2*weight), t_c_y(2*height))
-    t.setposition(t_c_x(2 * weight), t_c_y(2 * height))
+    t.dot()
+    t.penup()
+    x_max, y_max = t_c_x(2 * weight), 0 + t_c_y((delta + 1) * (2 * height // 3))
+    y_step = int((y_max - y0) / 10)
+    print(y_max - y0)
+    #vertical lines
+    for x in range(x0 + y_step, x_max, y_step):
+        t.setposition(x, y0)
+        t.pendown()
+        t.setposition(x, y_max)
+        t.penup()
+    #horizontal lines
+    for y in range(y0 + y_step, y_max, y_step):
+        t.setposition(x0, y)
+        t.pendown()
+        t.setposition(x_max, y)
+        t.penup()
+    print()
 draw_grid(0)
+draw_grid(1)
+draw_grid(2)
 t.done()
